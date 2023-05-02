54462f26a6505dad03b348eb286c5e7131d5ddda
youssef nabil <youssef.nabil.mustafa@gmail.com>
2023-05-02 19:52:00 +0300

    collections

--- src/main/scala/fundamentals/Section3FunctionalProgramming/Collections.scala
+++ src/main/scala/fundamentals/Section3FunctionalProgramming/Collections.scala
@@ -1,6 +1,8 @@
 package org.learning
 package fundamentals.Section3FunctionalProgramming
 
+import scala.util.Random
+
 object Collections extends App {
 
   /**
@@ -9,6 +11,11 @@ object Collections extends App {
    * 2. the function that is taken for flatmap is the  function applied as the normal map function except the result is flattened
    * 3. a list can have mixed types as the generic type of the standard list is A*
    * 4. for-comprehensions is the syntax sugar for the chain of map, flatmap and filters
+   * 5. the difference between arrays and list is immutability and arrays have the same implementation as java
+   * 6. difference between list and vectors are in performance the vectors are better in middle insertions and retrievals
+   * 7. lists: keeps reference to tail and updating an element in the middle takes long
+   * 8. depth of the tree is small and needs to replace an entire 32-element chunk
+   * 9. a -> b is sugar for (a, b) (tuples)
    */
   val list = List(1, 2, 3)
   println(list.head)
@@ -103,4 +110,89 @@ object Collections extends App {
   println(list.map {
     _ * 2
   })
+
+  // Seq
+  val aSequence = Seq(1, 3, 2, 4)
+  println(aSequence)
+  println(aSequence.reverse)
+  println(aSequence(2))
+  println(aSequence ++ Seq(7, 5, 6))
+  println(aSequence.sorted)
+
+  // Ranges
+  val aRange: Seq[Int] = 1 until 10
+  aRange.foreach(println)
+
+  (1 to 10).foreach(x => println("Hello"))
+
+  val aList = List(1, 2, 3)
+  val prepended = 42 +: aList :+ 89
+  println(prepended)
+
+  val apples5 = List.fill(5)("apple")
+  println(apples5)
+  println(aList.mkString("-|-"))
+
+  val numbersArr = Array(1, 2, 3, 4)
+  val threeElements = Array.ofDim[String](3)
+  threeElements.foreach(println)
+
+  numbersArr(2) = 0 // syntax sugar for numbers.update(2, 0)
+  println(numbers.mkString(" "))
+
+  // implicit conversions whenever possiable
+  val numbersSeq: Seq[Int] = numbers
+  println(numbersSeq)
+
+  val vector: Vector[Int] = Vector(1, 2, 3)
+  println(vector)
+
+  // calculating insertion types between lists and vectors
+  val maxRuns = 1000
+  val maxCapacity = 1000000
+
+  def getWriteTime(collection: Seq[Int]): Double = {
+    val r = new Random()
+    val times = for {
+      it <- 1 to maxRuns
+    } yield {
+      val currentTime = System.nanoTime()
+      collection.updated(r.nextInt(maxCapacity), r.nextInt())
+      System.nanoTime() - currentTime
+    }
+
+    times.sum * 1.0 / maxRuns
+  }
+
+  val numbersList = (1 to maxCapacity).toList
+  val numbersVector = (1 to maxCapacity).toVector
+
+  println(getWriteTime(numbersList))
+  println(getWriteTime(numbersVector))
+
+  val aTuple: (Int, String) = (2, "hello, Scala") // Tuple2[Int, String] = (Int, String)
+
+  println(aTuple._1) // 2
+  println(aTuple.copy(_2 = "goodbye Java"))
+  println(aTuple.swap) // ("hello, Scala", 2)
+
+  val aMap: Map[String, Int] = Map()
+
+  val phonebook = Map(("Jim", 555), "Daniel" -> 789, ("JIM", 9000)).withDefaultValue(-1)
+  println(phonebook)
+
+  println(phonebook.contains("Jim"))
+  println(phonebook("Mary"))
+
+  // add a pairing
+  val newPairing = "Mary" -> 678
+  val newPhonebook = phonebook + newPairing
+  println(newPhonebook)
+
+  // conversions to other collections
+  println(phonebook.toList)
+  //  println(List(("Daniel", 555)).toMap)
+  val names = List("Bob", "James", "Angela", "Mary", "Daniel", "Jim")
+  println(names.groupBy(name => name.charAt(0)))
+  println(names.groupBy(name => name.charAt(0) == 'J'))
 }
