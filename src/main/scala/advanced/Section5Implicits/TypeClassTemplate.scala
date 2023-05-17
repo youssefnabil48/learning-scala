31b74c9db551c1b42a19837e605cd32099f11b57
youssef nabil <youssef.nabil.mustafa@gmail.com>
2023-05-17 17:45:32 +0300

    template file

--- src/main/scala/advanced/Section5Implicits/TypeClassTemplate.scala
+++ src/main/scala/advanced/Section5Implicits/TypeClassTemplate.scala
@@ -1,18 +1,15 @@
 package org.learning
 package advanced.Section5Implicits
 
-object TypeClassTemplate extends App {
 
-  trait MyTypeClassTemplate[T] {
-    def action(value: T): String
-  }
-
-  object MyTypeClassTemplate {
-    def apply[T](implicit instance: MyTypeClassTemplate[T]): MyTypeClassTemplate[T] = instance
-  }
-
-  implicit object StringImplementor extends MyTypeClassTemplate[String] {
-    override def action(value: String): String = "desired string from action"
-  }
-
+trait MyTypeClassTemplate[T] {
+  def action(value: T): String
 }
+
+object MyTypeClassTemplate {
+  def apply[T](implicit instance: MyTypeClassTemplate[T]): MyTypeClassTemplate[T] = instance
+}
+
+//implicit object StringImplementor extends MyTypeClassTemplate[String] {
+//  override def action(value: String): String = "desired string from action"
+//}
